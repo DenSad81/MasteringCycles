a9d23c710224051e4308daa3624468339605545c
DenSad81 <tagil81@mail.ru>
2024-04-21 06:07:24 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MasteringCycles
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            string message;
+            int repititionCount;
+
+            Console.Write("Ваше сообщение: ");
+            message = (Console.ReadLine());
+            Console.Write("Количество повторений: ");
+            repititionCount = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 0; i < repititionCount; i++)
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
